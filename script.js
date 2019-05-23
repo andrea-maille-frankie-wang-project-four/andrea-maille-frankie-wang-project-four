c6081a17c905fcd20276bbf509e57c2c6397ae70
andreamaille <andrea.maille@gmail.com>
2019-05-22 21:50:33 -0400

    the dom is ours! HTML strings printing to page

--- script.js
+++ script.js
@@ -2,16 +2,39 @@
 const app = {};
 app.apiEndPoint = "https://api.walmartlabs.com/v1/search?";
 app.apiKey = "rk3f9sm8az24zz3wf7e5sesc";
+
+
+app.htmlStringMaking = function (dataArray){
+    $('.game-content').empty();
+    dataArray.forEach(data => {
+        const $card = $(`<div>`).addClass('inner-wrapper');
+        const $image = $(`<img class="product-image">`).attr('src',data.largeImage);
+        const $name = $(`<p class="product-name">${data.name}</p>`); 
+        const $userGuess = $(`<input type="number" id="userGuess" name="userGuess" placeholder="Guess the Price"></input>`);
+        const $userGuessLabel = $(`<label for="userGuess" class="visually-hidden">Guess the price of the item</label>`);
+        $card.append($image, $name, $userGuess, $userGuessLabel);
+        console.log($card);
+    
+        $('.game-content').append($card);
+    })
+    
+    const $submitGuess = $(`<button>Submit</button>`);
+
+    $('.game-content').append($submitGuess);
+}
+
 app.selectCategory = function () {
     $('.start-game').on('click', function(event){
         event.preventDefault();
-
         const selectedCategory = $('input[name=category]:checked').val();
         app.apiCall(selectedCategory);
-
         
+     
     })
 }
+
+
+
 app.randomChooseThree = function (dataArray) {
     const indexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
     const chosenIndexArray = [];
@@ -19,17 +42,17 @@ app.randomChooseThree = function (dataArray) {
         const chosenIndex = Math.floor((Math.random() * (19 - i)) + 1);
         chosenIndexArray.push(indexArray[chosenIndex]);
         indexArray[chosenIndex] = indexArray[20 - i];
-
+        
     }
-    console.log(chosenIndexArray);
-
-    const selectedItem = [];
+    const selectedItems = [];
     chosenIndexArray.forEach(index => {
-        selectedItem.push(dataArray[index]);
+        selectedItems.push(dataArray[index]);
     })
-    console.log(selectedItem);
+    
+    return(selectedItems);
 
 }
+
 app.apiCall = function(category) {
     $.ajax({
         url: app.apiEndPoint,
@@ -43,8 +66,11 @@ app.apiCall = function(category) {
 
         }
     }).then(function (data) {
+        const totalArray = data.items; 
         
-        app.randomChooseThree(data.items);
+        const threeItems = app.randomChooseThree(totalArray);
+        
+        app.htmlStringMaking(threeItems);
         
     }).fail(function(error){
         console.log("nah");
@@ -53,7 +79,7 @@ app.apiCall = function(category) {
 
 app.init = function() {
     app.selectCategory();
-
+    
 }
 
 $(function(){
