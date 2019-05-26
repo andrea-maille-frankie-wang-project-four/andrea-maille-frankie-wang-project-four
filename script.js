c83de935e4d86cf9705a48a092d76f0f76563eb6
FrankieWang <francescafranci@outlook.com>
2019-05-26 19:24:35 -0400

    function order refactored and add more tabindexes

--- script.js
+++ script.js
@@ -3,39 +3,79 @@ const app = {};
 //store api endpoint and apikey
 app.apiEndPoint = "https://api.walmartlabs.com/v1/search?";
 app.apiKey = "rk3f9sm8az24zz3wf7e5sesc";
+//we randomly pick three indexes. We call 25 products back from Api, but in case of error, we only select from 20 items, and use the indexes to get products for users' game
+app.randomChooseThree = function (dataArray) {
+    const indexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+    const chosenIndexArray = [];
+    for (i = 1; i <= 3; i++) {
+        const chosenIndex = Math.floor((Math.random() * (20 - i)) + 1);
+        chosenIndexArray.push(indexArray[chosenIndex]);
+        indexArray[chosenIndex] = indexArray[20 - i];
+    }
+    const selectedItems = [];
+    chosenIndexArray.forEach(index => {
+        selectedItems.push(dataArray[index]);
+    })
+    return selectedItems;
+}
 //print gaming interface using data from api call to DOM (with html strings and append them accordingly)
 app.htmlStringMaking = function (dataArray){
     $('.game-content').empty();
-    let counter = 1;
     for(i=0;i<dataArray.length;i++){
         const data = dataArray[i];
-        const $card = $('<fieldset>').addClass('card').attr(`tabindex`, `${2+counter}`);
-        counter++
-        const $productInfo = $('<div>').addClass('product-info').attr(`tabindex`,`${2 + counter}`);
-        counter++
+        const $card = $('<fieldset>').addClass('card').attr(`tabindex`, `${2+i*5+1}`);
+        const $productInfo = $('<div>').addClass('product-info');
         const $imageContainer = $('<div>').addClass('image-container');
         const $image = $(`<img class="product-image">`).attr('src',data.largeImage).attr("alt", "");
-        const $name = $(`<p class="product-name">${data.name}</p>`).attr(`tabindex`,`${2 + counter}`);
-        counter++
-        const $userGuessLabel = $(`<label for="user-guess" class="visually-hidden" aria-hidden="true">Place your guess here</label>`).attr(`tabindex`,`${2 + counter}`);
-        counter++
-        const $userGuess = $(`<input type="text" id="user-guess" name="user-guess" aira-hidden="true" placeholder="$0.00"></input>`).attr(`tabindex`, `${2 + counter}`);
-        counter++
+        const $name = $(`<p class="product-name">${data.name}</p>`).attr(`tabindex`,`${2 +i*5+2}`);
+        const $userGuessLabel = $(`<label for="user-guess" class="visually-hidden" aria-hidden="true">Place your guess here</label>`).attr(`tabindex`,`${2 +i*5+3}`);
+        const $userGuess = $(`<input type="text" id="user-guess" name="user-guess" aira-hidden="true" placeholder="$0.00"></input>`).attr(`tabindex`, `${2+i*5+4}`);
         const $gameResults = $('<div>').addClass(`game-results game-results-${i + 1}`).attr({
-            "tabindex":`${2+counter}`,
+            "tabindex":`${2+i*5+5}`,
             "aria-live":"polite",
             "role":"status"
-
         })
-        counter++
-        //comment testing
         $imageContainer.append($image);
         $productInfo.append($imageContainer, $name, $userGuessLabel, $userGuess);
         $card.append($productInfo, $gameResults);
         $('.game-content').append($card);
     }
-    const $submitGuess = $(`<button class="submit-form">Submit</button>`);
+    const $submitGuess = $(`<button class="submit-form">Submit</button>`).attr(`tabindex`,`18`);
     $('.game-content').append($submitGuess).addClass('game-shown');
+}
+//ajax calling api and process the raw data to make it more usable, then we call randomChooseThree method to get three products, changing currency to Canadian dollars, display game interface for users. if the api call fails, show an alert for users.
+app.apiCall = function (category) {
+    $.ajax({
+        url: app.apiEndPoint,
+        method: 'GET',
+        dataType: 'jsonp',
+        data: {
+            apiKey: app.apiKey,
+            query: category,
+            sort: "bestseller",
+            numItems: 25,
+
+        }
+    }).then(function (data) {
+        const totalArrayRaw = data.items;
+        const totalArray = totalArrayRaw.filter(item => item.salePrice !== undefined);
+        const threeItems = app.randomChooseThree(totalArray);
+        //the realPriceArray property will hold a retail price array scoped on the app object for later usages
+        app.realPriceArray = [];
+        threeItems.forEach(item => {
+            app.realPriceArray.push(+(item.salePrice * 1.35).toFixed(2));
+        })
+        app.htmlStringMaking(threeItems);
+    }).fail(function (error) {
+        Swal.fire({
+            title: 'Aaah',
+            background: '#ffe438',
+            text: '😢Sorry, failed to get API data back, please check back later.',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#349052',
+        })
+    })
+    return true;
 }
 //using an event listener to store users' category choice and scroll down to game interface after api call has finished. The game content won't load again unless users finish the game. If users click the button without choosing a category, an alert will prompt them to select one.
 app.selectCategory = function () {
@@ -43,9 +83,8 @@ app.selectCategory = function () {
         event.preventDefault();
         const category = $('input[name=category]:checked').val();
         if (category !== undefined && $('.game-content').hasClass("game-shown") === false){
-            const selectedCategory = $('input[name=category]:checked').val();
             async function scrollDownApi() {
-                const status = await app.apiCall(selectedCategory);
+                const status = await app.apiCall(category);
                 if (status === true) {
                     $('html, body').animate({
                         scrollTop: $('.game-content').offset().top
@@ -68,56 +107,6 @@ app.selectCategory = function () {
         }
     })
 }
-//we randomly pick three indexes. We call 25 products back from Api, but in case of error, we only select from 20 items, and use the indexes to get products for users' game
-app.randomChooseThree = function (dataArray) {
-    const indexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-    const chosenIndexArray = [];
-    for (i = 1; i <= 3; i++) {
-        const chosenIndex = Math.floor((Math.random() * (20 - i)) + 1);
-        chosenIndexArray.push(indexArray[chosenIndex]);
-        indexArray[chosenIndex] = indexArray[20 - i];
-    }
-    const selectedItems = [];
-    chosenIndexArray.forEach(index => {
-        selectedItems.push(dataArray[index]);
-    })
-    return selectedItems ;
-}
-//ajax calling api and process the raw data to make it more usable, then we call randomChooseThree method to get three products, changing currency to Canadian dollars, display game interface for users. if the api call fails, show an alert for users.
-app.apiCall = function(category) {
-    $.ajax({
-        url: app.apiEndPoint,
-        method: 'GET',
-        dataType: 'jsonp',
-        data :{
-            apiKey: app.apiKey,
-            query:category,
-            sort:"bestseller",
-            numItems:25,
-
-        }
-    }).then(function (data) {
-        const totalArrayRaw = data.items; 
-        const totalArray = totalArrayRaw.filter(item=>item.salePrice!==undefined);
-        const threeItems = app.randomChooseThree(totalArray);
-        //it will hold a retail price array scoped on the app object for later usages
-        app.realPriceArray = [];
-        threeItems.forEach(item=>{
-         app.realPriceArray.push(+(item.salePrice * 1.35).toFixed(2));
-        })
-        console.log(app.realPriceArray);
-        app.htmlStringMaking(threeItems);
-    }).fail(function(error){
-        Swal.fire({
-            title: 'Aaah',
-            background: '#ffe438',
-            text: '😢Sorry, failed to get API data back, please check back later.',
-            confirmButtonText: 'OK',
-            confirmButtonColor: '#349052',
-        })
-    })
-    return true;
-}
 //after users submit the form, store users' input. Then, check if users have completed the form correctly, if not, prompt them to do so with an alert; if yes, render the results for them including the actual price and the difference between their guess and the sale price, providing them with a reset button at the same time.
 app.storeUserInput =function (){
     $("form.game-content").on("click", ".submit-form", function(event){
@@ -133,52 +122,54 @@ app.storeUserInput =function (){
             }
         }
         if (notNumber === false) {
+            //hide the submit button from users after they submit their guess so they can only replay the game by clicking the reset button
             $(".submit-form").addClass("hide");
             for (let i = 0; i < userGuessArray.length; i++) {
                 const userGuessDecimal = +userGuessArray[i].toFixed(2);                
                 //if users' guess the price exactly right, tell users they win
                 if (userGuessDecimal === app.realPriceArray[i]) {
                     const $emotion = $(`<p><i aria-hidden="true" class="far fa-laugh-squint"></i></p>`); 
-                    const $feedbackSentence =$(`<p>YOU GUESS THE RIGHT PRICE! AMAZING!</p>`)
-                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $0!</p>`)
-                    const $retailPrice = $(`<p class="retail-price">Retail Price:${app.realPriceArray[i]}</p>`)
+                    const $feedbackSentence =$(`<p>YOU GUESS THE RIGHT PRICE! AMAZING!</p>`).attr( `tabindex`, 18+i*3+1);
+                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $0!</p>`).attr(`tabindex`,18+i*3+2);
+                    const $retailPrice = $(`<p class="retail-price">Retail Price:$${app.realPriceArray[i]}</p>`).attr(`tabindex`, 18+i*3+3);
                     $(`.game-results-${i+1}`).append($emotion, $feedbackSentence, $priceDifference, $retailPrice);
                     //if users'  guess is five dollars lower than actual price, give them positive feedback
                 } else if (app.realPriceArray[i] > userGuessDecimal && app.realPriceArray[i] - userGuessDecimal <= 5) {
                     const priceComparison = (app.realPriceArray[i] - userGuessDecimal).toFixed(2);
                     const $emotion = $(`<p><i aria-hidden="true" class="far fa-smile-beam"></i></p>`);
-                    const $feedbackSentence = $(`<p>You are only less than 5 dollars away!Good job!</p>`)
-                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`)
-                    const $retailPrice = $(`<p class="retail-price">Retail Price:${app.realPriceArray[i]}</p>`)
+                    const $feedbackSentence = $(`<p>You are only less than 5 dollars away!Good job!</p>`).attr(`tabindex`, 18+i*3+1);
+                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`).attr(`tabindex`,18+i*3+2);
+                    const $retailPrice = $(`<p class="retail-price">Retail Price:$${app.realPriceArray[i]}</p>`).attr(`tabindex`, 18+i*3+3);
                     $(`.game-results-${i + 1}`).append($emotion, $feedbackSentence, $priceDifference, $retailPrice);
                     //if users' guess is less than ten dollars away from actual price, semi-positive feedback for them
                 } else if (app.realPriceArray[i] > userGuessDecimal && app.realPriceArray[i] - userGuessDecimal <= 10) {
                     const priceComparison = (app.realPriceArray[i] - userGuessDecimal).toFixed(2);
                     const $emotion = $(`<p><i aria-hidden="true" class="far fa-grin"></i></p>`);
-                    const $feedbackSentence = $(`<p>You are only less than 10 dollars away! Not bad</p>`)
-                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`)
-                    const $retailPrice = $(`<p class="retail-price">Retail Price:${app.realPriceArray[i]}</p>`)
+                    const $feedbackSentence = $(`<p>You are only less than 10 dollars away! Not bad</p>`).attr(`tabindex`, 18+i*3+1);
+                    const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`).attr(`tabindex`,18+i*3+2);
+                    const $retailPrice = $(`<p class="retail-price">Retail Price:$${app.realPriceArray[i]}</p>`).attr(`tabindex`, 18+i*3+3);
                     $(`.game-results-${i + 1}`).append($emotion, $feedbackSentence, $priceDifference, $retailPrice);
                     //if users' guess is over or more than ten dollars lower than actual price, 🙁
                 } else {
                     if (app.realPriceArray[i] < userGuessDecimal) {
                         const priceComparison = (userGuessDecimal - app.realPriceArray[i]).toFixed(2);
                         const $emotion = $(`<p><i aria-hidden="true" class="far fa-frown"></i></p>`);
-                        const $feedbackSentence = $(`<p>You went over the retail price!</p>`)
-                        const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`)
-                        const $retailPrice = $(`<p class="retail-price">Retail Price:${app.realPriceArray[i]}</p>`)
+                        const $feedbackSentence = $(`<p>You went over the retail price!</p>`).attr(`tabindex`, 18+i*3+1);
+                        const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`).attr(`tabindex`,18+i*3+2);
+                        const $retailPrice = $(`<p class="retail-price">Retail Price:$${app.realPriceArray[i]}</p>`).attr(`tabindex`, 18+i*3+3);
                         $(`.game-results-${i + 1}`).append($emotion, $feedbackSentence, $priceDifference, $retailPrice);
                     } else {
                         const priceComparison = (app.realPriceArray[i] - userGuessDecimal).toFixed(2);
                         const $emotion = $(`<p><i aria-hidden="true" class="far fa-meh"></i></p>`);
-                        const $feedbackSentence = $(`<p>You are really far away!</p>`)
-                        const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`)
-                        const $retailPrice = $(`<p class="retail-price">Retail Price:${app.realPriceArray[i]}</p>`)
+                        const $feedbackSentence = $(`<p>You are really far away!</p>`).attr(   `tabindex`, 18+i*3+1);
+                        const $priceDifference = $(`<p class="price-difference-match">Price Difference: $${priceComparison}</p>`).attr(`tabindex`,18+i*3+2);
+                        const $retailPrice = $(`<p class="retail-price">Retail Price:$${app.realPriceArray[i]}</p>`).attr(`tabindex`, 18+i*3+3);
                         $(`.game-results-${i + 1}`).append($emotion, $feedbackSentence, $priceDifference, $retailPrice);
                     }
                 }    
             }
-            $(".game-content").append("<button class='replay'>Replay</button>");
+            const $replayButton =$("<button class='replay'>Replay</button>").attr(`tabindex`,`28`);
+            $(".game-content").append($replayButton);
         } else {
             Swal.fire({
                 title: 'Oops',
